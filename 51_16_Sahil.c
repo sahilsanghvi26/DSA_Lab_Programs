1d9f8e81a61455f30f10dfca0d66f1964528b2db
sahilsanghvi26 <140705346+sahilsanghvi26@users.noreply.github.com>
2023-10-04 13:02:03 +0530

    Create 51_16_Sahil.c

---
+++ 51_16_Sahil.c
@@ -0,0 +1,82 @@
+/*
+Name = Harsh Chaudhari
+Roll No. = 07
+SE-IT (sem 3)
+2023-24
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+    printf("%d pushed to the stack.\n", value);
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack is empty. Cannot pop.\n");
+        return;
+    }
+    struct Node* temp = top;
+    top = top->next;
+    int poppedValue = temp->data;
+    free(temp);
+    printf("%d popped from the stack.\n", poppedValue);
+}
+
+void display() {
+    struct Node* temp = top;
+    if (temp == NULL) {
+        printf("Stack is empty.\n");
+        return;
+    }
+    printf("Stack: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\nStack Operations:\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("Exiting program.\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
+}
