3119f42bbcd54296caf02db31f4e4165b02ada11
sahilsanghvi26 <140705346+sahilsanghvi26@users.noreply.github.com>
2023-10-04 12:51:33 +0530

    Create 51_05_Sahil.c

---
+++ 51_05_Sahil.c
@@ -0,0 +1,39 @@
+/*
+Name = Harsh Chaudhari 
+Roll No. = 07
+SE-IT (sem 3)
+2023-24
+*/
+
+#include<stdio.h>
+
+
+int maxim(int arr[], int n)
+{
+    int max=arr[0];
+    for(int i=0; i<n; i++)
+    {
+        if(max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void main(){
+    
+    int n;
+    printf("enter the length of the array:");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("enter the array values:");
+    for(int i=0; i<n; i++)
+    {
+        printf("enter %d th value: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+   
+    printf("the maximum of the array is:%d", maxim(arr, 5));
+}
