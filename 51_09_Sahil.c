1d2e3ab6d7e0695245946c36d60720a7d13f61c6
sahilsanghvi26 <140705346+sahilsanghvi26@users.noreply.github.com>
2023-10-04 12:55:39 +0530

    Create 51_09_Sahil.c

---
+++ 51_09_Sahil.c
@@ -0,0 +1,42 @@
+/*
+name = Harsh Chaudhari
+Roll No. = 07
+SE-IT (sem 3)
+2023-24
+*/
+
+#include<stdio.h>
+void main()
+{
+    
+    int n;
+    printf("enter the number of elements you want in array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter the elements: ");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    int counter =0;
+    int temp;
+    for(int i=0; i<n; i++)
+    {
+        temp =0;
+        /*checking that an elemnt is present how many times in array by temporary variable(temp)*/
+        for(int j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+                counter++;
+                temp++;
+            }
+        }
+        if(temp>1){
+           
+            counter-=temp;
+        }
+    }
+    /*printing the duplicate elements counter*/
+    printf("number of duplicate elements are: %d", counter);
+}
