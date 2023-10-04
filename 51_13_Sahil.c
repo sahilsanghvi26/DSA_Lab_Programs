86073f17587b49b804df68506c033870d74281b5
sahilsanghvi26 <140705346+sahilsanghvi26@users.noreply.github.com>
2023-10-04 12:58:36 +0530

    Create 51_13_Sahil.c

---
+++ 51_13_Sahil.c
@@ -0,0 +1,101 @@
+/*
+Name = Harsh Chaudhari
+Roll No. = 07
+SE-IT (sem 3)
+2023-24
+*/
+#include <stdio.h>
+#define MAX_SIZE 5
+
+int queue[MAX_SIZE];
+int front = -1, rear = -1;
+
+void enqueue(int item) {
+    if ((front == 0 && rear == MAX_SIZE - 1) || (rear == (front - 1) % (MAX_SIZE - 1))) {
+        printf("Queue is full. Cannot enqueue %d\n", item);
+    } else if (front == -1) {
+        front = rear = 0;
+        queue[rear] = item;
+    } else if (rear == MAX_SIZE - 1 && front != 0) {
+        rear = 0;
+        queue[rear] = item;
+    } else {
+        rear++;
+        queue[rear] = item;
+    }
+}
+
+int dequeue() {
+    if (front == -1) {
+        printf("Queue is empty. Cannot dequeue\n");
+        return -1;
+    }
+    int item = queue[front];
+    if (front == rear) {
+        front = rear = -1;
+    } else if (front == MAX_SIZE - 1) {
+        front = 0;
+    } else {
+        front++;
+    }
+    return item;
+}
+
+void display() {
+    if (front == -1) {
+        printf("Queue is empty.\n");
+        return;
+    }
+    printf("Queue elements: ");
+    if (rear >= front) {
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+    } else {
+        for (int i = front; i < MAX_SIZE; i++) {
+            printf("%d ", queue[i]);
+        }
+        for (int i = 0; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice, item;
+
+    do {
+        printf("\nCircular Queue Menu:\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Display\n");
+        printf("4. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter an element to enqueue: ");
+                scanf("%d", &item);
+                enqueue(item);
+                break;
+            case 2:
+                item = dequeue();
+                if (item != -1) {
+                    printf("Dequeued item: %d\n", item);
+                }
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("Exiting the program.\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
