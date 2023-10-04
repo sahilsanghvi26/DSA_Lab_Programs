0b4d48354c95914c97e1b5e10a41ee5db1b30d7a
sahilsanghvi26 <140705346+sahilsanghvi26@users.noreply.github.com>
2023-10-04 13:02:49 +0530

    Create 51_17_Sahil.c

---
+++ 51_17_Sahil.c
@@ -0,0 +1,89 @@
+/*
+Name = Harsh Chaudhari
+Roll No. = 07
+SE-IT (sem 3)
+2023-24
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node *f = NULL;
+struct Node *r = NULL;
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *ptr){
+    printf("Printing the elements of this linked list\n");
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+void enqueue(int val){
+    struct Node *n = (struct Node *) malloc(sizeof(struct Node));
+    if(n==NULL){
+        printf("Queue is Full");
+    }
+    else{
+        n->data = val;
+        n->next = NULL;
+        if(f==NULL){
+            f=r=n;
+        }
+        else{
+            r->next = n;
+            r=n;
+        }
+    }
+}
+
+int dequeue(){
+    int val = -1;
+    struct Node *ptr = f;
+    if(f==NULL){
+        printf("Queue is Empty\n");
+    }
+    else{
+        f = f->next;
+        val = ptr->data;
+        free(ptr);
+    }
+    return val;
+}
+int main() {
+    int choice, value;
+    while (1) {
+        printf("\nQueue Operations:\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Display\n");
+        printf("4. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to enqueue: ");
+                scanf("%d", &value);
+                enqueue( value);
+                break;
+            case 2:
+                printf("%d is removed from the Queue",dequeue());
+                break;
+            case 3:
+                linkedListTraversal(f);
+                break;
+            case 4:
+                printf("Exiting program.\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
+}
